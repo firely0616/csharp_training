09e957b843aa0ca73198bad18dcbc05787aae148
gohots <gohots0616@gmail.com>
2023-07-18 17:41:10 +0300

    Задание №3. Созданы вспомогательные методы и классы

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    internal class GroupData
+    {
+        private string name;
+        private string footer;
+        private string header;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Footer
+        {   get { return footer; } 
+            set {  footer = value; } 
+        }
+        public string Header
+        {   get { return header; }
+            set { header = value; }
+        }  
+    }
+}
