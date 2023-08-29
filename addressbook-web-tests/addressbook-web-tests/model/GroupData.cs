88bf4850b91319c5173548bcdf27bf2a21cf135f
gohots <gohots0616@gmail.com>
2023-08-29 19:02:08 +0300

    Добавил подключение к БД

--- addressbook-web-tests/addressbook-web-tests/model/GroupData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/GroupData.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToDB.Mapping;
 
 namespace WebAddressbookTests
 {
+    [Table(Name =  "group_list")]
     public class GroupData : IEquatable<GroupData>, IComparable<GroupData>
     {
         public GroupData()
@@ -16,18 +18,22 @@ namespace WebAddressbookTests
         {
             Name = name;
         }
+        [Column(Name = "group_id"), PrimaryKey, Identity]
         public string Id
         {
             get; set;
         }
+        [Column(Name = "group_name")]
         public string Name
         {
             get; set;
         }
+        [Column(Name = "group_footer")]
         public string Footer
         {
             get; set;
         }
+        [Column(Name = "group_header")]
         public string Header
         {
             get; set;
